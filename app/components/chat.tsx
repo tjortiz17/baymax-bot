7e30190669954904fee328e6b51f7ad8ba9910ed
Tyler Ortiz <tjortiz17@gmail.com>
2025-01-03 15:40:33 -0800

    code cleanup

--- app/components/chat.tsx
+++ app/components/chat.tsx
@@ -3,7 +3,6 @@
 import React, { useState, useEffect, useRef } from "react";
 import styles from "./chat.module.css";
 import Markdown from "react-markdown";
-// @ts-expect-error - no types for this yet
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const key = process.env.NEXT_PUBLIC_API_KEY;
@@ -31,43 +30,21 @@ const BaymaxMessage = ({ text }: { text: string }) => {
   );
 };
 
-// const CodeMessage = ({ text }: { text: string }) => {
-//   return (
-//     <div className={styles.codeMessage}>
-//       {text.split("\n").map((line, index) => (
-//         <div key={index}>
-//           <span>{`${index + 1}. `}</span>
-//           {line}
-//         </div>
-//       ))}
-//     </div>
-//   );
-// };
-
 const Message = ({ role, text }: MessageProps) => {
   switch (role) {
     case "user":
       return <UserMessage text={text} />;
     case "baymax":
       return <BaymaxMessage text={text} />;
-    // case "code":
-    //   return <CodeMessage text={text} />;
     default:
       return null;
   }
 };
 
-// type ChatProps = {
-//   functionCallHandler?: (
-//     toolCall: RequiredActionFunctionToolCall
-//   ) => Promise<string>;
-// };
-
 const Chat = () => {
   const [userInput, setUserInput] = useState("");
   const [messages, setMessages] = useState([]);
   const [inputDisabled, setInputDisabled] = useState(false);
-  //const [threadId, setThreadId] = useState("");
 
   // automatically scroll to bottom of chat
   const messagesEndRef = useRef<HTMLDivElement | null>(null);
@@ -77,18 +54,6 @@ const Chat = () => {
   useEffect(() => {
     scrollToBottom();
   }, [messages]);
-
-  // create a new threadID when chat component created
-  // useEffect(() => {
-  //   const createThread = async () => {
-  //     const res = await fetch(`/api/assistants/threads`, {
-  //       method: "POST",
-  //     });
-  //     const data = await res.json();
-  //     setThreadId(data.threadId);
-  //   };
-  //   createThread();
-  // }, []);
 
   const sendMessage = async (text) => {
     let result = await chat.sendMessage(text);
@@ -100,24 +65,6 @@ const Chat = () => {
     setInputDisabled(false);
     scrollToBottom();
   };
-
-  // const submitActionResult = async (runId, toolCallOutputs) => {
-  //   const response = await fetch(
-  //     `/api/assistants/threads/${threadId}/actions`,
-  //     {
-  //       method: "POST",
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //       },
-  //       body: JSON.stringify({
-  //         runId: runId,
-  //         toolCallOutputs: toolCallOutputs,
-  //       }),
-  //     }
-  //   );
-  //   const stream = AssistantStream.fromReadableStream(response.body);
-  //   handleReadableStream(stream);
-  // };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -131,123 +78,6 @@ const Chat = () => {
     setInputDisabled(true);
     scrollToBottom();
   };
-
-  /* Stream Event Handlers */
-
-  // textCreated - create new assistant message
-  // const handleTextCreated = () => {
-  //   appendMessage("assistant", "");
-  // };
-
-  // textDelta - append text to last assistant message
-  // const handleTextDelta = (delta) => {
-  //   if (delta.value != null) {
-  //     appendToLastMessage(delta.value);
-  //   };
-  //   if (delta.annotations != null) {
-  //     annotateLastMessage(delta.annotations);
-  //   }
-  // };
-
-  // imageFileDone - show image in chat
-  // const handleImageFileDone = (image) => {
-  //   appendToLastMessage(`\n![${image.file_id}](/api/files/${image.file_id})\n`);
-  // }
-
-  // toolCallCreated - log new tool call
-  // const toolCallCreated = (toolCall) => {
-  //   if (toolCall.type != "code_interpreter") return;
-  //   appendMessage("code", "");
-  // };
-
-  // toolCallDelta - log delta and snapshot for the tool call
-  // const toolCallDelta = (delta, snapshot) => {
-  //   if (delta.type != "code_interpreter") return;
-  //   if (!delta.code_interpreter.input) return;
-  //   appendToLastMessage(delta.code_interpreter.input);
-  // };
-
-  // handleRequiresAction - handle function call
-  // const handleRequiresAction = async (
-  //   event: AssistantStreamEvent.ThreadRunRequiresAction
-  // ) => {
-  //   const runId = event.data.id;
-  //   const toolCalls = event.data.required_action.submit_tool_outputs.tool_calls;
-  //   // loop over tool calls and call function handler
-  //   const toolCallOutputs = await Promise.all(
-  //     toolCalls.map(async (toolCall) => {
-  //       const result = await functionCallHandler(toolCall);
-  //       return { output: result, tool_call_id: toolCall.id };
-  //     })
-  //   );
-  //   setInputDisabled(true);
-  //   submitActionResult(runId, toolCallOutputs);
-  // };
-
-  // handleRunCompleted - re-enable the input form
-  // const handleRunCompleted = () => {
-  //   setInputDisabled(false);
-  // };
-
-  // const handleReadableStream = (stream: AssistantStream) => {
-  //   // messages
-  //   stream.on("textCreated", handleTextCreated);
-  //   stream.on("textDelta", handleTextDelta);
-
-  //   // image
-  //   stream.on("imageFileDone", handleImageFileDone);
-
-  //   // code interpreter
-  //   stream.on("toolCallCreated", toolCallCreated);
-  //   stream.on("toolCallDelta", toolCallDelta);
-
-  //   // events without helpers yet (e.g. requires_action and run.done)
-  //   stream.on("event", (event) => {
-  //     if (event.event === "thread.run.requires_action")
-  //       handleRequiresAction(event);
-  //     if (event.event === "thread.run.completed") handleRunCompleted();
-  //   });
-  // };
-
-  /*
-    =======================
-    === Utility Helpers ===
-    =======================
-  */
-
-  // const appendToLastMessage = (text) => {
-  //   setMessages((prevMessages) => {
-  //     const lastMessage = prevMessages[prevMessages.length - 1];
-  //     const updatedLastMessage = {
-  //       ...lastMessage,
-  //       text: lastMessage.text + text,
-  //     };
-  //     return [...prevMessages.slice(0, -1), updatedLastMessage];
-  //   });
-  // };
-
-  // const appendMessage = (role, text) => {
-  //   setMessages((prevMessages) => [...prevMessages, { role, text }]);
-  // };
-
-  // const annotateLastMessage = (annotations) => {
-  //   setMessages((prevMessages) => {
-  //     const lastMessage = prevMessages[prevMessages.length - 1];
-  //     const updatedLastMessage = {
-  //       ...lastMessage,
-  //     };
-  //     annotations.forEach((annotation) => {
-  //       if (annotation.type === 'file_path') {
-  //         updatedLastMessage.text = updatedLastMessage.text.replaceAll(
-  //           annotation.text,
-  //           `/api/files/${annotation.file_path.file_id}`
-  //         );
-  //       }
-  //     })
-  //     return [...prevMessages.slice(0, -1), updatedLastMessage];
-  //   });
-    
-  // }
 
   return (
     <div className={styles.chatContainer}>
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,5 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-// import Warnings from "./components/warnings";
-// import { assistantId } from "./assistant-config";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
